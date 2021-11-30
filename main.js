6ec8d75b08dda9c9746e8c8b002d51b586d16703
SangYong-Jeong <mmaf@naver.com>
2021-11-30 21:23:54 +0900

    vue관련 내용들 공부중 vue-cli

--- main.js
+++ main.js
@@ -1,26 +1,37 @@
 new Vue({
   el: '#app',
   data: {
-    name: '키메라',
-    list: [
-      { id: 1, name: '슬라임', hp: 100 },
-      { id: 2, name: '고블린', hp: 200 },
-      { id: 3, name: '드래곤', hp: 500 },
-    ],
+    show: true,
   },
   methods: {
-    // 추가 버튼을 클릭할 때의 핸들러
-    doAdd: function () {
-      // 리스트 내부에서 가장 큰 ID 추출하기
-      var max = this.list.reduce(function (a, b) {
-        return a > b.id ? a : b.id;
-      }, 0);
-      // 새로운 몬스터를 리스트에 추가하기
-      this.list.push({
-        id: max + 1, // 현재 최대 ID에 1을 더해서 유니크 ID로 사용하기
-        name: this.name, // 현재 입력 양식의 값
-        hp: 500,
-      });
+    // Enter
+    beforeEnter: function (el) {
+      console.log('before-enter');
+    },
+    enter: function (el, done) {
+      console.log('enter');
+      setTimeout(done, 1000);
+    },
+    afterEnter: function (el) {
+      console.log('after-enter');
+    },
+    enterCancelled: function (el) {
+      console.log('enter-cancelled');
+    },
+    // Leave
+    beforeLeave: function (el) {
+      console.log('before-leave');
+    },
+    leave: function (el, done) {
+      console.log('leave');
+      setTimeout(done, 1000);
+    },
+    afterLeave: function (el) {
+      console.log('after-leave');
+    },
+    // v-show와 함께 사용하는 경우에만 leaveCancelled를 사용할 수 있습니다.
+    leaveCancelled: function (el) {
+      console.log('leave-cancelled');
     },
   },
 });
